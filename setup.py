1cadcd71870d8e748cfe71fd431ba2ff53b159cf
Ed Summers <ehs@pobox.com>
2015-09-25 20:40:35 -0400

    test python3

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+from setuptools import setup, Command
+
+class PyTest(Command):
+    """
+    A command to convince setuptools to run pytests.
+    """
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import pytest
+        errno = pytest.main("test.py")
+        sys.exit(errno)
+
+requirements = open('requirements.txt').read().split()
+
+setup(
+    name = 'ndnp_iiif',
+    version = '0.0.1',
+    url = 'http://github.com/umd-mith/ndnp_iiif',
+    author = 'Ed Summers',
+    author_email = 'ehs@pobox.com',
+    py_modules = ['ndnp_iiif'],
+    scripts = ['ndnp_iiif.py'],
+    description = 'convert ndnp data to iiif',
+    test_suite = 'test',
+    install_requires = requirements,
+    cmdclass = {'test': PyTest},
+    tests_require = ['pytest'],
+)
